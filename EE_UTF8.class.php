9e4ba6942c55bae6e69a1162b3c666aa1d6d6ceb
Michael Nelson <michael@eventespresso.com>
2014-10-31 12:16:50 -0700

    [touch:6955]{t:120}because it has aspects o fan addon (specifically that it needs to put the font files in uploads) made it an addon that also registers a template variation

---
+++ EE_UTF8.class.php
@@ -0,0 +1,98 @@
+<?php if ( ! defined( 'EVENT_ESPRESSO_VERSION' )) { exit(); }
+/**
+ * ------------------------------------------------------------------------
+ *
+ * Class  EE_UTF8
+ *
+ * @package			Event Espresso
+ * @subpackage		espresso-utf8
+ * @author			    Brent Christensen
+ * @ version		 	$VID:$
+ *
+ * ------------------------------------------------------------------------
+ */
+// define the plugin directory path and URL
+define( 'EE_UTF8_BASENAME', plugin_basename( EE_UTF8_PLUGIN_FILE ));
+define( 'EE_UTF8_PATH', plugin_dir_path( __FILE__ ));
+define( 'EE_UTF8_URL', plugin_dir_url( __FILE__ ));
+define( 'EE_UTF8_ADMIN', EE_UTF8_PATH . 'admin' . DS . 'utf8' . DS );
+Class  EE_UTF8 extends EE_Addon {
+
+	/**
+	 * class constructor
+	 */
+	public function __construct() {
+	}
+
+	public static function register_addon() {
+		// register addon via Plugin API
+		EE_Register_Addon::register(
+			'UTF8',
+			array(
+				'version' 					=> EE_UTF8_VERSION,
+				'min_core_version' => '4.3.0.dev.000',
+				'main_file_path' 				=> EE_UTF8_PLUGIN_FILE,
+				'autoloader_paths' => array(
+					'EE_UTF8' 						=> EE_UTF8_PATH . 'EE_UTF8.class.php',
+				),
+				'module_paths' 		=> array( EE_UTF8_PATH . 'EED_UTF8.module.php' ),
+				// if plugin update engine is being used for auto-updates. not needed if PUE is not being used.
+				'pue_options'			=> array(
+					'pue_plugin_slug' => 'espresso_utf8',
+					'plugin_basename' => EE_UTF8_BASENAME,
+					'checkPeriod' => '24',
+					'use_wp_update' => FALSE,
+					),
+			)
+		);
+		add_action( 'EE_Brewing_Regular___messages_caf',  array( 'EE_UTF8', 'add_utf8_variations' ) );
+	}
+
+	public static function add_utf8_variations() {
+		if ( ! class_exists( 'EE_Register_Messages_Template_Variations' ) ) {
+			return;
+		}
+
+		//setup variations array for all known message types.
+		$message_types = array(
+			'receipt', 'invoice',
+			);
+		$vtions = array();
+		foreach ( $message_types as $message_type ) {
+			$vtions[$message_type] = array(
+							'default_utf8' => __('Simple UTF8', 'ee-new-variations-test' ),
+							);
+		}
+
+		$variations_setup = array(
+			'base_path' => EE_UTF8_PATH . 'variations/',
+			'base_url' => EE_UTF8_URL . 'variations/',
+			'variations' => array(
+				'default' => array(
+					'html' => $vtions,
+					'pdf' => $vtions
+					)
+				)
+			);
+		EE_Register_Messages_Template_Variations::register( 'utf8', $variations_setup );
+	}
+
+
+
+	/**
+	 * 	additional_admin_hooks
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public function additional_admin_hooks() {
+
+	}
+
+
+
+
+
+}
+// End of file EE_UTF8.class.php
+// Location: wp-content/plugins/espresso-utf8/EE_UTF8.class.php
---
+++ espresso-utf8.php
@@ -0,0 +1,51 @@
+<?php
+/*
+  Plugin Name: Event Espresso - UTF8 (EE4.x+)
+  Plugin URI: http://www.eventespresso.com
+  Description: The Event Espresso UTF8 adds NEW stuff to Event Espresso. Compatible with Event Espresso 4.x or higher
+  Version: 1.0.0.dev.000
+  Author: Event Espresso
+  Author URI: http://www.eventespresso.com
+  Copyright 2014 Event Espresso (email : support@eventespresso.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA02110-1301USA
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for WordPress
+ *
+ * @ package		Event Espresso
+ * @ author			Event Espresso
+ * @ copyright	(c) 2008-2014 Event Espresso  All Rights Reserved.
+ * @ license		http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
+ * @ link				http://www.eventespresso.com
+ * @ version	 	EE4
+ *
+ * ------------------------------------------------------------------------
+ */
+define( 'EE_UTF8_VERSION', '1.0.0.dev.000' );
+define( 'EE_UTF8_PLUGIN_FILE',  __FILE__ );
+function load_espresso_utf8() {
+if ( class_exists( 'EE_Addon' )) {
+	// utf8 version
+	require_once ( plugin_dir_path( __FILE__ ) . 'EE_UTF8.class.php' );
+	EE_UTF8::register_addon();
+}
+}
+add_action( 'AHEE__EE_System__load_espresso_addons', 'load_espresso_utf8' );
+
+// End of file espresso_utf8.php
+// Location: wp-content/plugins/espresso-utf8/espresso_utf8.php
